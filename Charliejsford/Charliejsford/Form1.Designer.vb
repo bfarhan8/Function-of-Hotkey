c0de5aa7e44105e789c6bef14a98dd6d0a4d4782
Farhan Basheer <farhanfb6@outlook.com>
2021-02-08 09:59:26 +0500

    Commit All

---
+++ Charliejsford/Charliejsford/Form1.Designer.vb
@@ -0,0 +1,70 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.NotifyIcon1 = New System.Windows.Forms.NotifyIcon(Me.components)
+        Me.SuspendLayout()
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(49, 61)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(69, 13)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Set the URL."
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(149, 57)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(160, 20)
+        Me.TextBox1.TabIndex = 2
+        Me.TextBox1.Text = "http://www.example.com/"
+        '
+        'NotifyIcon1
+        '
+        Me.NotifyIcon1.Text = "Charlie"
+        Me.NotifyIcon1.Visible = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(361, 146)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Label2)
+        Me.KeyPreview = True
+        Me.Name = "Form1"
+        Me.Text = "Main Page"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Label2 As Label
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents NotifyIcon1 As NotifyIcon
+End Class
---
+++ Charliejsford/Charliejsford/Form1.vb
@@ -0,0 +1,66 @@
+﻿'This Program will wait for a key to press in our case it will wait for the F8 key to be press
+'Then it will copy the highlighted text (Outside and Inside of the form) and will concatinate the text with a webaddress'
+Imports System.Net
+Imports Charliejsford.Hotkey
+Public Class Form1
+
+    'Global Variable to Store the Highlighted value'
+
+    Dim HighlightedValue As String
+    'This Chunk of code will register the F8 key as a main key for the Program'
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Hotkey.registerHotkey(Me, "f8", Hotkey.KeyModifier.None)
+    End Sub
+    'This sub will trigger the Hotkey Sub Code in the Hotkey.vb Class'
+
+    Protected Overrides Sub WndProc(ByRef m As System.Windows.Forms.Message)
+        If m.Msg = Hotkey.WM_HOTKEY Then
+            Hotkey.handleHotKeyEvent(m.WParam)
+
+            'After pressing the F8 key It will copy the highlighted data from anywhere and store it to the clipboard'
+            If Clipboard.ContainsText Then
+                HighlightedValue = My.Computer.Clipboard.GetData(DataFormats.Text).ToString
+
+                'This part will concatinate the web address(Specified in Textbox1) with the clipboard value'
+
+                Try
+                    Dim webAddress As String = TextBox1.Text + HighlightedValue
+                    Process.Start(webAddress)
+                Catch ex As Exception
+                    MessageBox.Show("Error in Program" + ex.ToString())
+                End Try
+            End If
+        End If
+        MyBase.WndProc(m)
+
+
+
+    End Sub
+
+    Private Sub Form1_Resize(sender As Object, e As EventArgs) Handles MyBase.Resize
+
+        If Me.WindowState = FormWindowState.Minimized Then
+            NotifyIcon1.Visible = True
+            NotifyIcon1.Icon = SystemIcons.Application
+            NotifyIcon1.BalloonTipIcon = ToolTipIcon.Info
+            NotifyIcon1.BalloonTipTitle = "Running In the Background"
+            NotifyIcon1.BalloonTipText = "Application is running in the Background. Double click it to maximize the form"
+            NotifyIcon1.ShowBalloonTip(50000)
+            Me.Hide()
+            ShowInTaskbar = True
+        End If
+    End Sub
+
+    Private Sub NotifyIcon1_DoubleClick(ByVal sender As Object, ByVal e As System.EventArgs) Handles NotifyIcon1.DoubleClick
+        Me.Show()
+        ShowInTaskbar = True
+        Me.WindowState = FormWindowState.Normal
+        NotifyIcon1.Visible = False
+
+    End Sub
+    'System wide hotkey event handling
+
+
+
+
+End Class
